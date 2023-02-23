fedd921766bccfec7dfcbcf7f7a4a3f3c24307fa
Ema Halimi <ehalimi@na.edu>
2023-02-23 16:04:01 -0600

    successfully implemented client side graphql

--- server/server.js
+++ server/server.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const { authMiddleware } = require('./utils/auth');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 
-//import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-
-//import out typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-
-//create a new Apollo server and pass in our schema
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 //Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
